1d3e81d00f4d2d68267f643720419bfea7f1d03f
isaac lynch <thebunnyarcher@gmail.com>
2021-10-02 19:48:58 -0400

    no skele code, explanation in desc.

---
+++ exercise27/src/test/java/baseline/Solution27Test.java
@@ -0,0 +1,24 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution27Test {
+
+    @Test
+    void validateFirstName() {
+    }
+
+    @Test
+    void validateLastName() {
+    }
+
+    @Test
+    void validateZipCode() {
+    }
+
+    @Test
+    void validateEmployeeID() {
+    }
+}
