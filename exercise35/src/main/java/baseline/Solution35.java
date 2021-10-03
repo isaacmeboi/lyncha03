3e293b6df99bd32142a1b84b6dd5a5762e61a05a
isaac lynch <thebunnyarcher@gmail.com>
2021-10-03 19:39:07 -0400

    no skele code, desc for explanation

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,40 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution35 {
+    String name;
+    ArrayList<String> nameList = new ArrayList<>();//stores names
+
+    public void addName(){
+        Scanner input = new Scanner(System.in);
+
+        //start a do-while loop to take name
+        do {
+            System.out.println("Enter a name:");
+            name= input.nextLine();
+            //add the name only if it isn't an empty input
+            if(!name.isBlank() && !name.isEmpty())
+                nameList.add(name);
+        }while(!name.isBlank() && !name.isEmpty());
+
+    }
+    public void pickWinner(){
+        //generate a random index
+        Random random = new Random();
+        int randomIndex = random.nextInt(nameList.size());
+
+        //pick the winner at the randomIndex of nameList
+        System.out.println("The winner is... "+nameList.get(randomIndex));
+    }
+
+    public static void main(String[] args) {
+
+        Solution35 ca = new Solution35();
+        ca.addName();
+        ca.pickWinner();
+    }
+
+}
