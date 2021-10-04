3aa11659bb91c59ffb3098c9a0863b19a7238107
isaac lynch <thebunnyarcher@gmail.com>
2021-10-03 20:50:16 -0400

    no skele code, explanation in desc. (2nd commit)

--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -1,23 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Isaac Lynch
+ */
 package baseline;
 
 import java.util.Scanner;
 
 public class Solution26 {
+
+    double apr;
+    double b;
+    double p;
     //does the math
-    public static int calculateMonthsUntilPaidOff(double b, double apr, double p) {
+    public int calculateMonthsUntilPaidOff(double b, double apr, double p) {
 
         apr = apr / 365;
         return (int) Math.ceil((-1 / 30.0) * Math.log(1 + b / p * (1 - Math.pow(1 + apr, 30))) / Math.log(1 + apr));
 
     }
-
-    public static void main(String[] args) {
+    public void print(){
 
 
         Scanner scanner = new Scanner(System.in);
-        double b;
-        double apr;
-        double p;
+
 
         System.out.print("What is your balance? ");
         b = scanner.nextDouble();
@@ -31,6 +36,22 @@ public class Solution26 {
 
         int months = calculateMonthsUntilPaidOff(b, apr, p);
         System.out.println("It will take " + months + " months to pay off this card.");
+    }
+    public static void main(String[] args) {
+        Solution26 ca = new Solution26();
+        ca.print();
+    }
+
+    public void setApr(double apr) {
+        this.apr = apr;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public void setP(double p) {
+        this.p = p;
     }
 }
 
--- exercise26/src/test/java/baseline/Solution26Test.java
+++ exercise26/src/test/java/baseline/Solution26Test.java
@@ -8,5 +8,12 @@ class Solution26Test {
 
     @Test
     void calculateMonthsUntilPaidOff() {
+        Solution26 ca = new Solution26();
+        ca.setApr(.12);
+        ca.setB(5000);
+        ca.setP(100);
+        int expected = 70;
+        int actual = ca.calculateMonthsUntilPaidOff(5000,.12,100);
+        assertEquals(expected,actual);
     }
 }
